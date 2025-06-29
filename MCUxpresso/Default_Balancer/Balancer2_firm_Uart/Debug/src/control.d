a19ad480f09b43b23297b3d8c199c430b46092ed
SHIMADA Nobutaka <nobutaka.shimada@gmail.com>
2025-06-29 16:28:45 +0900

    firmware files configured

--- MCUxpresso/Default_Balancer/Balancer2_firm_Uart/Debug/src/control.d
+++
@@ -1,9 +0,0 @@
-src/control.o src/control.d: ../src/control.c \
- /home/okui/mcuxpresso/BeuatoBalancer2/Balancer2/MCUxpresso/Balancer2_firm_Uart/lib/Balancer2.h \
- /home/okui/mcuxpresso/BeuatoBalancer2/Balancer2/MCUxpresso/Balancer2_firm_Uart/lib/LPC13xx.h \
- /home/okui/mcuxpresso/BeuatoBalancer2/Balancer2/MCUxpresso/Balancer2_firm_Uart/lib/core_cm3.h \
- /home/okui/mcuxpresso/BeuatoBalancer2/Balancer2/MCUxpresso/Balancer2_firm_Uart/lib/system_LPC13xx.h
-/home/okui/mcuxpresso/BeuatoBalancer2/Balancer2/MCUxpresso/Balancer2_firm_Uart/lib/Balancer2.h:
-/home/okui/mcuxpresso/BeuatoBalancer2/Balancer2/MCUxpresso/Balancer2_firm_Uart/lib/LPC13xx.h:
-/home/okui/mcuxpresso/BeuatoBalancer2/Balancer2/MCUxpresso/Balancer2_firm_Uart/lib/core_cm3.h:
-/home/okui/mcuxpresso/BeuatoBalancer2/Balancer2/MCUxpresso/Balancer2_firm_Uart/lib/system_LPC13xx.h:
--- MCUxpresso/Default_Balancer/Balancer2_firm_Uart/Debug/src/subdir.mk
+++ MCUxpresso/Default_Balancer/Balancer2_firm_Uart/Debug/src/subdir.mk
@@ -23,7 +23,7 @@ OBJS += \
 src/%.o: ../src/%.c src/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -D__USE_CMSIS=Balancer2_lib -DDEBUG -D__CODE_RED -D__REDLIB__ -I"/home/okui/mcuxpresso/BeuatoBalancer2/Balancer2/MCUxpresso/Balancer2_firm_Uart/lib" -I"/home/okui/mcuxpresso/BeuatoBalancer2/Balancer2/MCUxpresso/Balancer2_firm_Uart/inc" -O1 -g -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fmacro-prefix-map="$(<D)/"= -mcpu=cortex-m3 -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	arm-none-eabi-gcc -D__USE_CMSIS=Balancer2_lib -DDEBUG -D__CODE_RED -D__REDLIB__ -I"/Users/shimadalocal/src/Balancer2/MCUxpresso/Default_Balancer/Balancer2_firm_Uart/lib" -I"/Users/shimadalocal/src/Balancer2/MCUxpresso/Default_Balancer/Balancer2_firm_Uart/inc" -O1 -g -gdwarf-4 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fmacro-prefix-map="$(<D)/"= -mcpu=cortex-m3 -mthumb -D__REDLIB__ -fstack-usage -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
